f1a469589cde9f3eb7bccf9b1e72a4c30d1efe8a
lucas3g <lucassilva080397@gmail.com>
2020-04-07 22:21:32 -0300

    finaliznado backend

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+
+app.use(routes);
+app.listen(3000);
---
+++ backend/src/routes.js
@@ -0,0 +1,39 @@
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
+const Post = require('./models/Post');
+
+routes.get('/posts', async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
+  console.log(req.file);
+  const { originalname: name, size, key, location: url = '' } = req.file;
+  const { title, subt, titledesc, desc } = JSON.parse(req.body.description);
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url,
+    title,
+    subt,
+    titledesc,
+    desc
+  });
+  return res.json(post);
+});
+
+routes.delete('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
+})
+
+module.exports = routes;
